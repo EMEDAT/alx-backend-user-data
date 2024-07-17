c168fc1ed96d9361ef79b4078db3025fc60459a5
EMEDAT <emedattitus24@gmail.com>
2024-07-17 06:30:56 +0000

    okay

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -1,20 +1,19 @@
 #!/usr/bin/env python3
 """
-Encrypting passwords
+Hashing with bcrypt
 """
-
-
 import bcrypt
 
 
 def hash_password(password: str) -> bytes:
     """
-    Salted pass generation
+    hash password
     """
     return bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
 
 
 def is_valid(hashed_password: bytes, password: str) -> bool:
-    """ is valid?
+    """
+    Validate password
     """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
